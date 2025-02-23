2df09f5092250b3fd4b71d6d6008a25c11dfe016
toki-ai <lucngan2004@gmail.com>
2025-02-23 17:04:36 +0700

    slash screen and lottie animation

---
+++ app/src/main/java/com/example/mockproject/SlashActivity.java
@@ -0,0 +1,34 @@
+package com.example.mockproject;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SlashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_slash);
+        LottieAnimationView lottieAni = findViewById(R.id.iv_icon_splash);
+        lottieAni.playAnimation();
+        lottieAni.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                Intent intent = new Intent(SlashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
