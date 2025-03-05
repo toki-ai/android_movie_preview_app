ac0e145c8466bb37dd6eeced16a2d18c78f2328b
toki-ai <lucngan2004@gmail.com>
2025-03-05 15:05:22 +0700

    optimize api by add params

--- app/src/main/java/com/example/mockproject/api/MovieApiService.java
+++ app/src/main/java/com/example/mockproject/api/MovieApiService.java
@@ -10,26 +10,13 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieApiService {
-    @GET("movie/popular")
-    Call<MovieResponse> getPopularMovies(
+    @GET("movie/{category}")
+    Call<MovieResponse> getMoviesByCategory(
+            @Path("category") String category,
             @Query("api_key") String apiKey,
             @Query("page") int page
     );
-    @GET("movie/top_rated")
-    Call<MovieResponse> getTopRatedMovies(
-            @Query("api_key") String apiKey,
-            @Query("page") int page
-    );
-    @GET("movie/upcoming")
-    Call<MovieResponse> getUpcomingMovies(
-            @Query("api_key") String apiKey,
-            @Query("page") int page
-    );
-    @GET("movie/now_playing")
-    Call<MovieResponse> getNowPlayingMovies(
-            @Query("api_key") String apiKey,
-            @Query("page") int page
-    );
+
     @GET("movie/{id}")
     Call<Movie> getMovieDetail(
             @Path("id") int movieId,
--- app/src/main/java/com/example/mockproject/fragment/ListMoviesFragment.java
+++ app/src/main/java/com/example/mockproject/fragment/ListMoviesFragment.java
@@ -125,21 +125,23 @@ public class ListMoviesFragment extends Fragment  {
         SharedPreferences sharedPreferences = requireContext().getSharedPreferences(SHARE_KEY, Context.MODE_PRIVATE);
         String movieType = sharedPreferences.getString(KEY_MOVIE_TYPE, "Popular Movies");
         Call<MovieResponse> call;
+        String category = "";
         switch (movieType) {
             case TYPE_TOP_RATED:
-                call = movieApiService.getTopRatedMovies(API_KEY, page);
+                category = "top_rated";
                 break;
             case TYPE_UPCOMING:
-                call = movieApiService.getUpcomingMovies(API_KEY, page);
+                category = "upcoming";
                 break;
             case TYPE_NOW_PLAYING:
-                call = movieApiService.getNowPlayingMovies(API_KEY, page);
+                category = "now_playing";
                 break;
             case TYPE_POPULAR:
             default:
-                call = movieApiService.getPopularMovies(API_KEY, page);
+                category = "popular";
                 break;
         }
+        call = movieApiService.getMoviesByCategory(category, API_KEY, 1);
 
         call.enqueue(new Callback<>() {
             @Override
